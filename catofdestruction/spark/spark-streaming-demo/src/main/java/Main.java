b25615ec08c1956dab3bc565fc0ecb2fa587509a
CatOfDestruction <decatofdestruction@gmail.com>
2018-05-14 11:36:13 +0800

    replace log4j.properties file with Logger.getLogger("org.apache.spark").setLevel(Level.WARN)

--- catofdestruction/spark/spark-streaming-demo/src/main/java/Main.java
+++ catofdestruction/spark/spark-streaming-demo/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 /**
  * Created by DeCatOfDestruction on 2018/5/10.
  */
@@ -8,6 +11,8 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+
+        Logger.getLogger("org.apache.spark").setLevel(Level.WARN);
 
         WordCount wordCount = new WordCount();
         wordCount.wordCountWithFile("hdfs://localhost:9000/user/DeCatOfDestruction/wordcount/resource/");
--- catofdestruction/spark/word-count-with-spark/src/main/java/Main.java
+++ catofdestruction/spark/word-count-with-spark/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 /**
  * Created by DeCatOfDestruction on 2018/4/26.
  */
@@ -7,6 +10,8 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+
+        Logger.getLogger("org.apache.spark").setLevel(Level.WARN);
 
         WordCount wordCount = new WordCount();
         wordCount.wordCountWithFile("hdfs://localhost:9000/user/DeCatOfDestruction/wordcount/resource/wordCountFile.txt");
